e66f81cf2c66086636c613eb5ebd0661b8206cf3
justin38 <justinya07@gmail.com>
2017-05-28 16:25:15 -0700

    add

--- ui.R
+++ ui.R
@@ -2,25 +2,23 @@ fluidPage(
   
   # Copy the line below to make a text input box
   textInput("text", label = h3("Search Artists"), value = "Enter text..."),
-  
-  hr(),
   fluidRow(column(3, verbatimTextOutput("value"))),
+  
+  
   
   
   radioButtons("radio", label = h3("Continents"),
-               choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+               choices = list("Asia" = 1, "Europe" = 2, "Latin America" = 3, "United States and Canada" = 4), 
                selected = 1),
-  
-  hr(),
+    
   fluidRow(column(3, verbatimTextOutput("value"))),
   
-    # Copy the line below to make a select box 
-    selectInput("select", label = h3("Countries"), 
-                choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
-                selected = 1),
     
-    hr(),
-    fluidRow(column(3, verbatimTextOutput("value")))
+
+  selectInput("select", label = h3("Countries"), 
+              choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+              selected = 1),
+  fluidRow(column(3, verbatimTextOutput("value")))
     
   
   
